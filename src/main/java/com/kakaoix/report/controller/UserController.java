eb06eb4e5cfae2c1dd53796a8bf3713f796a012d
bghgu <ektmf1993@gmail.com>
2018-08-29 15:57:33 +0900

    controller setting

---
+++ src/main/java/com/kakaoix/report/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.kakaoix.report.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ds on 2018-08-29.
+ */
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+}
